77ae6784cbfa1cca8695b03578e549f1048a91d9
yure-nogueira <pedro.nogueira@terceirizados.medgrupo.com.br>
2025-08-11 17:54:20 -0300

    teste

--- medtango-components/medtango-components.esm.js
+++ medtango-components/medtango-components.esm.js
@@ -1,2 +1,32 @@
-import{p as a,b as t}from"./p-B4O2AaR1.js";export{s as setNonce}from"./p-B4O2AaR1.js";import{g as o}from"./p-DQuL1Twl.js";var r=()=>{const t=import.meta.url;const o={};if(t!==""){o.resourcesUrl=new URL(".",t).href}return a(o)};r().then((async a=>{await o();return t([["p-026bad15",[[257,"mt-grid-row",{cols:[2]}]]],["p-28f6275a",[[262,"mt-title-h1",{produto:[513],especialidade:[513],ano:[513]}]]]],a)}));
+import { p as a, b as t } from './p-B4O2AaR1.js';
+export { s as setNonce } from './p-B4O2AaR1.js';
+import { g as o } from './p-DQuL1Twl.js';
+var r = () => {
+  const t = import.meta.url;
+  const o = {};
+  if (t !== '') {
+    o.resourcesUrl = new URL('.', t).href;
+  }
+  return a(o);
+};
+r().then(async (a) => {
+  await o();
+  return t(
+    [
+      ['p-026bad15', [[257, 'mt-grid-row', { cols: [2] }]]],
+      [
+        'p-28f6275a',
+        [
+          [
+            262,
+            'mt-title-h1',
+            { produto: [513], especialidade: [513], ano: [513] }
+          ]
+        ]
+      ]
+    ],
+    a
+  );
+});
+alert('ABABABABAB');
 //# sourceMappingURL=medtango-components.esm.js.map
